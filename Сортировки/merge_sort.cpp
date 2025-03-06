fa0982bc505ffcc93e13e062bf9b5ca9c78d53b5
moera112 <andrlavr16@gmail.com>
2025-03-06 11:53:02 +0300

    Update merge_sort.cpp

--- Сортировки/merge_sort.cpp
+++ Сортировки/merge_sort.cpp
@@ -1,70 +1,57 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
-#define IN for(int i = 0; i < n; i++) cin >> arr[i];
-#define OUT for(int  i = 0; i < n; i++) cout << arr[i] << ' ';
-
-vector<int> merge(vector<int>&left, vector<int>&right)
-{
-	vector<int> merged;
-
-	int left_index = 0, right_index = 0;
-
-	while(left_index < left.size() && right_index < right.size())
-		if (left[left_index] <= right[right_index])
-		{
-			merged.push_back(left[left_index++]);
-		}
-		else
-			merged.push_back(right[right_index++]);
-
-	while (left_index < left.size())
-	{
-		merged.push_back(left[left_index++]);
-	}
-
-	while (right_index < right.size())
-		merged.push_back(right[right_index++]);
-
-	return merged;
-}
-
-
 
 void merge_sort(vector<int>& arr)
 {
-	if (arr.size() <= 0)
-		return;
+	if (arr.size() <= 1)
+		return ;
 
-	vector <int> left, right;
+	vector<int> left, right;
+
 	int n = arr.size() / 2;
 
-	for (int i = 0; i < n; i++)
-		left.push_back(arr[i]);
-
-	for (int i = n; i < arr.size(); i++)
-		right.push_back(arr[i]);
+	for (int i = 0; i < arr.size(); i++)
+		if (i < n)
+			left.push_back(arr[i]);
+		else
+			right.push_back(arr[i]);
 
 	merge_sort(left);
 	merge_sort(right);
 
-	arr = merge(left, right);
+	int l_ind = 0, r_ind = 0, ind = 0;
 
-}
+	while(l_ind < left.size() && r_ind < right.size())
+		if (left[l_ind] < right[r_ind])
+		{
+			arr[ind] = left[l_ind];
+			ind++;
+			l_ind++;
+		}
+		else
+		{ 
+			arr[ind] = right[r_ind];
+			ind++;
+			r_ind++;
+		}
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	while (l_ind < left.size())
+	{
+		arr[ind] = left[l_ind];
+		ind++;
+		l_ind++;
+	}
 
-	int n;
-	cin >> n;
+	while (r_ind < right.size())
+	{
+		arr[ind] = right[r_ind];
+		ind++;
+		r_ind++;
+	}
 
-	vector<int> arr(n);
-	IN;
-	merge_sort(arr);
-	OUT;
 
 }
